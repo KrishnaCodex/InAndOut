f4621c289ada1a594af50945c872b4e8d3d4a9ea
Krishna Mishra <kuleen24@gmail.com>
2022-07-09 12:22:48 +0530

    AddedExpense Added Validation

--- InAndOut/Models/Expense.cs
+++ InAndOut/Models/Expense.cs
@@ -13,8 +13,11 @@ namespace InAndOut.Models
         public int Id { get; set; }
 
         [DisplayName("Expense")]
+        [Required(ErrorMessage ="Name should be there")]
         public string ExpenseName { get; set; }
 
+        [Required]
+        [Range(0,1000,ErrorMessage ="Expense should be less than 1000")]
         public int Amount { get; set; }
     }
 }
